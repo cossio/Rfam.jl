2d80454f573aff881f16d67da7a8ef30746d1617
Jorge FdCD <jfdecd@icloud.com>
2024-07-30 23:10:27 +0200

    kwargs

--- src/Rfam.jl
+++ src/Rfam.jl
@@ -1,9 +1,9 @@
 module Rfam
 
-using Downloads: download
-using Preferences: @set_preferences!, @load_preference
 import Gzip_jll
 import Tar
+using Downloads: download
+using Preferences: @set_preferences!, @load_preference
 #using CodecZlib: GzipDecompressorStream
 
 include("preferences.jl")
@@ -11,21 +11,29 @@ include("preferences.jl")
 # make the loading of RFAM files thread-safe
 const RFAM_LOCK = ReentrantLock()
 
-base_url() = "https://ftp.ebi.ac.uk/pub/databases/Rfam/$(get_rfam_version())"
-version_dir() = mkpath(joinpath(get_rfam_directory(), get_rfam_version()))
-fasta_dir() = mkpath(joinpath(version_dir(), "fasta_files"))
+function base_url(; rfam_version = get_rfam_version())
+    return "https://ftp.ebi.ac.uk/pub/databases/Rfam/$rfam_version"
+end
+
+function version_dir(; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
+    return mkpath(joinpath(rfam_dir, rfam_version))
+end
+
+function fasta_dir(; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
+    return mkpath(joinpath(version_dir(; rfam_dir, rfam_version), "fasta_files"))
+end
 
 """
     fasta_file(family_id)
 
 Returns local path to `.fasta` file of `family_id`.
 """
-function fasta_file(family_id::AbstractString)
+function fasta_file(family_id::AbstractString; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
     lock(RFAM_LOCK) do
-        local_path = joinpath(fasta_dir(), "$family_id.fa")
+        local_path = joinpath(fasta_dir(; rfam_dir, rfam_version), "$family_id.fa")
         if !isfile(local_path)
             @info "Downloading $family_id to $local_path ..."
-            rfam_base_url = base_url()
+            rfam_base_url = base_url(; rfam_version)
             url = "$rfam_base_url/fasta_files/$family_id.fa.gz"
             download(url, local_path * ".gz"; timeout = Inf)
             gunzip(local_path * ".gz")
@@ -39,12 +47,12 @@ end
 
 Returns the path to `Rfam.cm` file containing the covariance models of all the families.
 """
-function cm()
+function cm(; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
     lock(RFAM_LOCK) do
-        local_path = joinpath(version_dir(), "Rfam.cm")
+        local_path = joinpath(version_dir(; rfam_dir, rfam_version), "Rfam.cm")
         if !isfile(local_path)
             @info "Downloading Rfam.cm to $local_path ..."
-            rfam_base_url = base_url()
+            rfam_base_url = base_url(; rfam_version)
             download("$rfam_base_url/Rfam.cm.gz", "$local_path.gz"; timeout = Inf)
             gunzip(local_path * ".gz")
         end
@@ -57,12 +65,12 @@ end
 
 Returns the path to `Rfam.clanin`.
 """
-function clanin()
+function clanin(; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
     lock(RFAM_LOCK) do
-        local_path = joinpath(version_dir(), "Rfam.clanin")
+        local_path = joinpath(version_dir(; rfam_dir, rfam_version), "Rfam.clanin")
         if !isfile(local_path)
             @info "Downloading Rfam.clanin to $local_path ..."
-            rfam_base_url = base_url()
+            rfam_base_url = base_url(; rfam_version)
             download("$rfam_base_url/Rfam.clanin", "$local_path"; timeout = Inf)
         end
         return local_path
@@ -74,12 +82,12 @@ end
 
 Returns the path to `Rfam.seed`.
 """
-function seed()
+function seed(; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
     lock(RFAM_LOCK) do
-        local_path = joinpath(version_dir(), "Rfam.seed")
+        local_path = joinpath(version_dir(; rfam_dir, rfam_version), "Rfam.seed")
         if !isfile(local_path)
             @info "Downloading Rfam.seed to $local_path ..."
-            rfam_base_url = base_url()
+            rfam_base_url = base_url(; rfam_version)
             download("$rfam_base_url/Rfam.seed.gz", "$local_path.gz"; timeout = Inf)
             gunzip("$local_path.gz")
         end
@@ -92,12 +100,12 @@ end
 
 Returns the path to the `.seed_tree` of the family.
 """
-function seed_tree(family_id::AbstractString)
+function seed_tree(family_id::AbstractString; rfam_dir = get_rfam_directory(), rfam_version = get_rfam_version())
     lock(RFAM_LOCK) do
-        local_path = joinpath(version_dir(), "Rfam.seed_tree")
+        local_path = joinpath(version_dir(; rfam_dir, rfam_version), "Rfam.seed_tree")
         if !isdir(local_path)
             @info "Downloading Rfam.seed_tree.tar.gz to $local_path ..."
-            rfam_base_url = base_url()
+            rfam_base_url = base_url(; rfam_version)
             download("$rfam_base_url/Rfam.seed_tree.tar.gz", "$local_path.tar.gz"; timeout = Inf)
 
             # Rfam.seed_tree.tar.gz seems not to be really gunzipped, it's just a Tar archive.
